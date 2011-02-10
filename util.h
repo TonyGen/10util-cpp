dc8a3245ea0ecf1a8dfe94f4fa1ce7dae4b9b9b4
Tony Hannan <tony@10gen.com>
2011-02-09 23:04:45 -0500

    some new functions

--- util.h
+++ util.h
@@ -23,13 +23,23 @@ inline std::vector <std::string> argsVector (int argc, char* argv[]) {
 	return args;
 }
 
+template <class A, class B> B foldl (boost::function2<B,B,A> fun, B init, std::vector<A> list) {
+	B val = init;
+	for (unsigned i = 0; i < list.size(); i++)
+		val = fun (val, list[i]);
+	return val;
+}
+template <class A, class B> B foldl (B (*fun) (B, A), B init, std::vector<A> list) {
+	boost::function2<B,B,A> f = fun;
+	return foldl (f, init, list);
+}
+
 template <class A, class B> std::vector<B> fmap (boost::function1<B,A> fun, std::vector<A> list) {
 	std::vector<B> newList;
 	for (unsigned i = 0; i < list.size(); i++)
 		newList.push_back (fun (list[i]));
 	return newList;
 }
-
 template <class A, class B> std::vector<B> fmap (B (*fun) (A), std::vector<A> list) {
 	boost::function1<B,A> f = fun;
 	return fmap (f, list);
@@ -49,11 +59,23 @@ template <class A> std::vector<A> cycle (unsigned n, std::vector<A> items) {
 }
 
 /** Repeat item until N listed */
-template <class A> std::vector<A> repeat (unsigned n, A item) {
+template <class A> std::vector<A> replicate (unsigned n, A item) {
 	std::vector<A> list;
 	for (unsigned i = 0; i < n; i++)
 		list.push_back (item);
 	return list;
+}
+
+/** Run action N times collecting results */
+template <class A> std::vector<A> repeat (unsigned n, boost::function0<A> action) {
+	std::vector<A> list;
+	for (unsigned i = 0; i < n; i++)
+		list.push_back (action());
+	return list;
+}
+template <class A> std::vector<A> repeat (unsigned n, A (*action) ()) {
+	boost::function0<A> f = action;
+	return repeat (n, f);
 }
 
 /** pair elements from each list together, final list size equals size of smaller list, remaining elements in larger list are ignored */
@@ -63,6 +85,24 @@ template <class A, class B> std::vector< std::pair<A,B> > zip (std::vector<A> as
 	for (unsigned i = 0; i < n; i++)
 		pairs.push_back (std::make_pair (as[i], bs[i]));
 	return pairs;
+}
+
+template <class A> std::vector<A> intersperse (A sep, std::vector<A> list) {
+	if (list.empty()) return list;
+	std::vector<A> newList;
+	newList.push_back (list[0]);
+	for (unsigned i = 1; i < list.size(); i++) {
+		newList.push_back (sep);
+		newList.push_back (list[i]);
+	}
+	return newList;
+}
+
+inline std::string concat (std::vector<std::string> strings) {
+	std::stringstream ss;
+	for (unsigned i = 0; i < strings.size(); i++)
+		ss << strings[i];
+	return ss.str();
 }
 
 template <class T> std::ostream& operator<< (std::ostream& out, const std::vector<T>& list) {
